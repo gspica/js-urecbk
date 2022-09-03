f92967c43350735b93a35fb73eee4e1b00df8c9a
gspica <gerald.spica@gmail.com>
2022-09-03 12:22:04 -0700

    1 file modified

--- analytics.js
+++ analytics.js
@@ -12,6 +12,4 @@ export function sendToEmail(data) {
   console.log('Sent to email: ', data);
 }
 
-Observer.unsubscribe(sendToGoogleAnalytics, sendToCustomAnalytics);
-Observer.subscribe(sendToCustomAnalytics);
-Observer.subscribe(sendToEmail);
+Observer.subscribe(sendToCustomAnalytics, sendToCustomAnalytics, sendToEmail);
